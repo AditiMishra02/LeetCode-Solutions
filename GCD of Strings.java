e3885400efbe6c2eacc002ec2a325c52190cb38d
Aditi Mishra <83416767+AditiMishra02@users.noreply.github.com>
2024-02-08 17:49:07 +0530

    Add files via upload

---
+++ GCD of Strings.java
@@ -0,0 +1,48 @@
+
+/*For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+ 
+
+Example 1:
+
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+
+Example 2:
+
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+
+Example 3:
+
+Input: str1 = "LEET", str2 = "CODE"
+Output: ""
+ 
+
+Constraints:
+
+1 <= str1.length, str2.length <= 1000
+str1 and str2 consist of English uppercase letters. */
+
+
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+    if(!(str1 + str2).equals(str2 + str1)) {
+         return "";
+     }   
+     int commonLength = gcd(str1.length(),str2.length());
+     return str1.substring(0,commonLength );
+     }
+
+     private static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+  
+}
